e35def44acf540cc5ab5a42d66964cbd53050c9e
joapedu <joaoeduardobraga2@gmail.com>
2023-09-11 08:51:45 -0300

    feat: issue #3 resolve input clear

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
 <html lang="pt-br">
-        <p> &nbsp&nbsp&nbsp;</p><img src="assets/mantask.png" alt="logo" style="position: absolute; margin-left: 30%">
+        <img src="assets/mantask.png" alt="logo">
 </html>
--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 function adiciona() {
     const tarefa = document.getElementById('tarefa').value
 
+    //repetição dos elementos da lista
+    //if da tarefa existente dentro da lista(instância de procura)
+    //return com alert
+
     let listaDeTarefas = document.getElementById('listaDeTarefas')
 
     let listaTarefasAdicionadas = listaDeTarefas.innerHTML.split('<li>')
@@ -8,4 +12,6 @@ function adiciona() {
     let existeTarefa = listaTarefasAdicionadas.includes(tarefa+'</li>')
 
     listaDeTarefas.innerHTML = listaDeTarefas.innerHTML + `<li>${tarefa}</li>`
+
+    document.getElementById('tarefa').value='';
 }
